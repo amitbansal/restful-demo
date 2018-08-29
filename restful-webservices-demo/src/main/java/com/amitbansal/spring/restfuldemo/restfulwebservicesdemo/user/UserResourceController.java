fa8f92e15865d9545bf95eaad03a6f1b4fcfcdd4
DELLXPS12\amitb <amitbansal@gmail.com>
2018-08-28 22:01:55 -0400

    Implemented delete functionality

--- restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/user/UserResourceController.java
+++ restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/user/UserResourceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,14 @@ public class UserResourceController {
 			throw new UserNotFoundException("id is"+id);
 		}
 		return user;
+	}
+	
+	@DeleteMapping(path = "/user/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user =  service.deleteById(id);
+		if(user == null){
+			throw new UserNotFoundException("id is"+id);
+		}		
 	}
 	
 	@PostMapping("/users")
