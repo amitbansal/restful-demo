636b3d8428183a44dd9d7935caa2e94339f4bed1
DELLXPS12\amitb <amitbansal@gmail.com>
2018-08-31 19:18:46 -0400

    Internationalization simplified

--- restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/helloworld/HelloWorldController.java
+++ restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/helloworld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,8 +35,8 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path = "/hello-world-internationalized")
-	public String helo(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helo() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 }
