a220d1a158e8a928cf84face25a71100d20fa084
DELLXPS12\amitb <amitbansal@gmail.com>
2018-09-02 20:20:25 -0400

    versioning and security

---
+++ restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package com.amitbansal.spring.restfuldemo.restfulwebservicesdemo.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+	public PersonV1() {
+		super();		
+	}
+}
