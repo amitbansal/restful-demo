3928562728685c0792372a7318ed7a5312e524dd
DELLXPS12\amitb <amitbansal@gmail.com>
2018-09-03 09:11:31 -0400

    Added JpaRepository implementation in the restful demo

--- restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/user/User.java
+++ restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/user/User.java
@@ -2,12 +2,20 @@ package com.amitbansal.spring.restfuldemo.restfulwebservicesdemo.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description ="All details about the user.")
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min=2, message ="Name should be atleast 2 chars")
 	@ApiModelProperty(notes = "Name should be atleast 2 chars")
---
+++ restful-webservices-demo/src/main/java/com/amitbansal/spring/restfuldemo/restfulwebservicesdemo/user/UserResourceJpaController.java
@@ -0,0 +1,71 @@
+package com.amitbansal.spring.restfuldemo.restfulwebservicesdemo.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+@RestController
+public class UserResourceJpaController {
+	
+	@Autowired	
+	private UserRepository userRepository;
+		
+	
+	@GetMapping(path = "/jpa/users")
+	public List<User> retrieveAllUsers() {
+		return userRepository.findAll();
+	}
+	@GetMapping(path = "/jpa/user/{id}")
+	public Resource<User> retrieveUser(@PathVariable int id) {
+		Optional<User> user =  userRepository.findById(id);
+		if(!user.isPresent()){
+			throw new UserNotFoundException("id is"+id);
+		}
+		Resource<User> resource = new Resource<User>(user.get());
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
+	}
+	
+	@DeleteMapping(path = "/jpa/user/{id}")
+	public void deleteUser(@PathVariable int id) {
+		Optional<User> user =  userRepository.findById(id);
+		if(!user.isPresent()){
+			throw new UserNotFoundException("id is"+id);
+		}
+		userRepository.deleteById(id);		
+		
+	}
+	
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+		User savedUser  = userRepository.save(user);
+		//created
+		//expose the URI of the created user in the response.
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId())
+		.toUri();
+		return ResponseEntity.created(location).build();
+		
+	}
+	
+	
+}
